78fdbea94916084b28e0853655546e7d967b25ca
MNZSRafael <135064113+MNZSRafael@users.noreply.github.com>
2023-12-03 22:29:57 -0300

    Adicionar arquivos de projeto.

---
+++ Livro.cs
@@ -0,0 +1,29 @@
+﻿class Livro
+    
+    {
+    public string NomeDoLivro { get; set; } 
+    public string Autor { get; set; }   
+    public string Editora { get; set; }
+    public int Paginas { get; set; }
+    public bool Disponivel { get; set; }
+    public string DescricaoResumida => $"O livro {NomeDoLivro} foi escrito por {Autor}.\n";
+
+
+
+    public void ExibirFichaCadastral() 
+    {
+        Console.WriteLine($"Título: {NomeDoLivro}");
+        Console.WriteLine($"Autor(a): {Autor}");
+        Console.WriteLine($"Editora: {Editora}");
+        Console.WriteLine($"nº páginas: {Paginas}");
+        Console.WriteLine(DescricaoResumida);
+        if (Disponivel)
+        {
+            Console.WriteLine("O livro encontra-se disponível.\n");
+        }
+
+        else { Console.WriteLine("O livro está esgotado.\n"); }
+       
+    }
+
+    }
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Livro livro1 = new Livro();
+livro1.NomeDoLivro = "Cem anos de solidão";
+livro1.Autor = "Gabriel García Marquez";
+livro1.Editora = "Record";
+livro1.Paginas = 396;
+livro1.Disponivel = true;
+
+Livro livro2 = new Livro();
+livro2.NomeDoLivro = "A metamorfose";
+livro2.Autor = "Franz Kakfa";
+livro2.Editora = "Record";
+livro2.Paginas = 96;
+livro2.Disponivel = false;
+
+livro1.ExibirFichaCadastral();
+livro2.ExibirFichaCadastral();
+
+
+
+Console.ReadLine();
+Console.Clear();
+Console.WriteLine("Aperte qualquer tecla para voltar");
+Console.ReadLine();
